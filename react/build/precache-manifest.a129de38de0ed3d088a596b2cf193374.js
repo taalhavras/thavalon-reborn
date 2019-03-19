41522f746b6198070e78e19781922dc1551d10ea
KaushikRaghuNimmagadda <kaushik_nimmagadda@brown.edu>
2019-03-19 03:25:11 +0000

    removed gradle build directory, kept react/build

---
+++ react/build/precache-manifest.a129de38de0ed3d088a596b2cf193374.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3bf00e585782373e1b601c07b513d85",
+    "url": "/static/media/Quicksand_Bold.c3bf00e5.otf"
+  },
+  {
+    "revision": "f79e1d8666e285dea10c",
+    "url": "/static/css/main.67631fc8.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "bf675df25a3c7c9aa8e2",
+    "url": "/static/js/2.bf675df2.chunk.js"
+  },
+  {
+    "revision": "0dae96901b999a3ce66c7fa728b78158",
+    "url": "/static/media/Yataghan-Regular.0dae9690.otf"
+  },
+  {
+    "revision": "f79e1d8666e285dea10c",
+    "url": "/static/js/main.f79e1d86.chunk.js"
+  },
+  {
+    "revision": "dd2da1d8f9d3944efe2797e1fa02e096",
+    "url": "/static/media/Quicksand_Book.dd2da1d8.otf"
+  },
+  {
+    "revision": "38c516075c4966be487ed354a1a8f1f0",
+    "url": "/static/media/Cardinal.38c51607.ttf"
+  },
+  {
+    "revision": "1950d3c1074310f40363fb019c9aa138",
+    "url": "/static/media/excalibur_by_psdeluxe_d8a6nxy-pre.1950d3c1.jpg"
+  },
+  {
+    "revision": "4cc4456358ec8a860e8fe2d01b8e12c5",
+    "url": "/static/media/Enchanted-Land.4cc44563.otf"
+  },
+  {
+    "revision": "42698ecb33e3ecfb1efbc10b4eca7753",
+    "url": "/index.html"
+  }
+];
