602f7c543f4c80aef9cd77639884b8912d905e77
KaushikRaghuNimmagadda <kaushik_nimmagadda@brown.edu>
2019-05-27 02:27:29 +0000

    starting statemachine

--- src/main/kotlin/livegame/LiveGame.kt
+++ src/main/kotlin/livegame/LiveGame.kt
@@ -4,15 +4,80 @@ import com.google.gson.JsonArray
 import com.google.gson.JsonElement
 import com.google.gson.JsonObject
 import io.ktor.http.cio.websocket.send
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import main.MessageType
 import main.THavalonUserSession
 import main.kotlin.thavalon.Game
 import java.lang.IllegalArgumentException
+import java.util.concurrent.CountDownLatch
 
 typealias PlayerInfo = Pair<JsonObject, THavalonUserSession>
 
 // Missions are just Sets of player names
 typealias Mission = Set<String>
+
+abstract class LiveGameState(open val g : LiveGame, respondsTo : Set<MessageType>) {
+    // message types we can respond to
+    private val respondsTo : Set<String> = respondsTo.map { it.toString() }.toSet()
+    // set of players who have already responded validly to our message. If
+    // they have, they cannot respond again
+    private val alreadyResponded : MutableSet<String> = HashSet()
+
+    // used to synchronize the onResponse function calls so they don't
+    // clobber each other. We should also synchronize using this before
+    // sending requests because we might get responses before we are in
+    // our call to nextState
+
+    // will be initialized by sendRequests since that'll tell us
+    // how many responses we will need
+    lateinit var cdl : CountDownLatch
+
+    abstract suspend fun sendRequests() : Unit
+
+    // checks to see if a response is valid, which would mean we have to take action
+    // this will be called from onResponse
+    fun validResponse(res : JsonObject) : Boolean {
+        return res.get("type").asString !in respondsTo || res.get("player").asString !in alreadyResponded
+    }
+
+    abstract suspend fun onResponse(res : JsonObject) : Unit
+
+    abstract suspend fun nextState() : LiveGameState
+
+    suspend fun advance() : Unit {
+        sendRequests()
+        val state = nextState()
+        g.setState(state)
+        state.advance()
+    }
+}
+
+class MissionOneState(override val g : LiveGame) : LiveGameState(g, setOf(MessageType.MISSION_ONE_PROPOSAL_RESPONSE)) {
+    override suspend fun sendRequests() {
+        // get first and second proposing players
+        val players = g.players
+        val firstProposingPlayer = players[players.size - 2]
+        val secondProposingPlayer = players[players.size - 1]
+        // send mission 1 proposal message to the two selected players
+        val msg = JsonObject()
+        msg.addProperty("type", MessageType.MISSION_ONE_PROPOSAL.toString())
+        // init cdl
+        cdl = CountDownLatch(2)
+        firstProposingPlayer.second.socket!!.send(msg.toString())
+        secondProposingPlayer.second.socket!!.send(msg.toString())
+    }
+
+    override suspend fun onResponse(res : JsonObject) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override suspend fun nextState(): LiveGameState {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
+
 
 /**
  * This class represents a remote live game running on the server.
@@ -30,7 +95,9 @@ class LiveGame(val game : Game, jsonifiedGame : JsonArray, playerSessions : List
     }
 
     // represents the state in our state machine. We will only accept incoming messages of this type
-    var currentState : MessageType =
+    // game starts waiting for mission one proposal responses
+    // var currentState : MessageType = MessageType.MISSION_ONE_PROPOSAL_RESPONSE
+    var currentState : LiveGameState = MissionOneState(this)
 
     init {
         assert(jsonifiedGame.size() == playerSessions.size)
@@ -42,6 +109,12 @@ class LiveGame(val game : Game, jsonifiedGame : JsonArray, playerSessions : List
             }
             throw IllegalArgumentException("Live game player without session")
         }
+
+
+        // start the first state
+        GlobalScope.launch {
+            currentState.advance()
+        }
     }
 
     suspend fun sendMessage(msg : JsonObject, name : String) {
@@ -51,6 +124,11 @@ class LiveGame(val game : Game, jsonifiedGame : JsonArray, playerSessions : List
 
     suspend fun sendToAll(msg : JsonObject) {
         players.map { it.second.socket!!.send(msg.toString()) }
+    }
+
+    suspend fun setState(state : LiveGameState) {
+        currentState = state
+        state.advance()
     }
 
 }
