ac5c0840a197eff6e24d90e02950eb310916e4c6
Grace Bramley-Simmons <gbramleysimmons@gmail.com>
2019-07-22 17:16:40 -0400

    frontend server

---
+++ old-frontend/build/precache-manifest.c74f8472c08fd590e95b55c704499ab2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c804fef2601ef617f70",
+    "url": "/static/css/main.3c640c2b.chunk.css"
+  },
+  {
+    "revision": "2c804fef2601ef617f70",
+    "url": "/static/js/main.2c804fef.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "8625990dcd17080ba74b",
+    "url": "/static/js/2.8625990d.chunk.js"
+  },
+  {
+    "revision": "38c516075c4966be487ed354a1a8f1f0",
+    "url": "/static/media/Cardinal.38c51607.ttf"
+  },
+  {
+    "revision": "dd2da1d8f9d3944efe2797e1fa02e096",
+    "url": "/static/media/Quicksand_Book.dd2da1d8.otf"
+  },
+  {
+    "revision": "c3bf00e585782373e1b601c07b513d85",
+    "url": "/static/media/Quicksand_Bold.c3bf00e5.otf"
+  },
+  {
+    "revision": "1950d3c1074310f40363fb019c9aa138",
+    "url": "/static/media/excalibur_by_psdeluxe_d8a6nxy-pre.1950d3c1.jpg"
+  },
+  {
+    "revision": "b80f3478f8e8cf856784062a8980581e",
+    "url": "/index.html"
+  }
+];
