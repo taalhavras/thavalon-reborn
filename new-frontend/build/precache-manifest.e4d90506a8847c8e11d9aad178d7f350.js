537abd2b48094a4c9c0b1dc2ccb9baeb4871111c
Grace Bramley-Simmons <gbramleysimmons@gmail.com>
2019-08-04 21:02:09 -0400

    stuff

---
+++ new-frontend/build/precache-manifest.e4d90506a8847c8e11d9aad178d7f350.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "1950d3c1074310f40363fb019c9aa138",
+    "url": "/static/media/excalibur_by_psdeluxe_d8a6nxy-pre.1950d3c1.jpg"
+  },
+  {
+    "revision": "dd2da1d8f9d3944efe2797e1fa02e096",
+    "url": "/static/media/Quicksand_Book.dd2da1d8.otf"
+  },
+  {
+    "revision": "c3bf00e585782373e1b601c07b513d85",
+    "url": "/static/media/Quicksand_Bold.c3bf00e5.otf"
+  },
+  {
+    "revision": "38c516075c4966be487ed354a1a8f1f0",
+    "url": "/static/media/Cardinal.38c51607.ttf"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "01e4d511d1a161581467",
+    "url": "/static/js/main.01e4d511.chunk.js"
+  },
+  {
+    "revision": "9de84767f6734658ced8",
+    "url": "/static/js/2.9de84767.chunk.js"
+  },
+  {
+    "revision": "01e4d511d1a161581467",
+    "url": "/static/css/main.31718c25.chunk.css"
+  },
+  {
+    "revision": "f57172fbc4e226b3e2fee5a507ce86e3",
+    "url": "/index.html"
+  }
+];
