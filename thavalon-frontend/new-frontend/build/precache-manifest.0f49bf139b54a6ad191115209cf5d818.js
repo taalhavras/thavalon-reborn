a7a439fb242854db4c0c7cc1f04f7a97e3e92581
Grace Bramley-Simmons <gbramleysimmons@gmail.com>
2019-08-04 21:02:09 -0400

    stuff

---
+++ thavalon-frontend/new-frontend/build/precache-manifest.0f49bf139b54a6ad191115209cf5d818.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d4bb3b2eff0ccc831d5347e4d8d364f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a68b6118b7f5458b168a",
+    "url": "/static/css/main.482c31d3.chunk.css"
+  },
+  {
+    "revision": "5f5b2946a63b7fdfee47",
+    "url": "/static/js/2.7302b93f.chunk.js"
+  },
+  {
+    "revision": "a68b6118b7f5458b168a",
+    "url": "/static/js/main.326db6a2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fa90b5193a512e36b8af8979a15d63b1",
+    "url": "/static/media/left-trees.fa90b519.svg"
+  },
+  {
+    "revision": "7e1fbb66bce521b416e15a69d458a80a",
+    "url": "/static/media/right-trees.7e1fbb66.svg"
+  }
+]);
